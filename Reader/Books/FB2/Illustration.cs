e21f032bad8d5180f7e25982bbb75025ee54c98c
KaMZeSs <vladd267@gmail.com>
2022-06-28 22:21:30 +0300

    WIP

--- Reader/Books/FB2/Illustration.cs
+++ Reader/Books/FB2/Illustration.cs
@@ -8,7 +8,24 @@ namespace Reader.Books.FB2
 {
     internal class Illustration
     {
-        private byte[] data;
+        public struct Img
+        {
+            public string Type;
+            public byte[] Data;
+        }
+
+        private Img image;
         private string name;
+
+        public static Img GetIllustrationByName(Illustration[] illustrations, string name)
+        {
+            var found = from res in illustrations.AsParallel() 
+                        where res.name.Equals(name) select res.image;
+            if (found is null)
+            {
+                throw new KeyNotFoundException($"There is no illustration which name is {name}");
+            }
+            return found.First();
+        }
     }
 }
