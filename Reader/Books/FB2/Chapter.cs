48fe230597818f6757c7c2eb81eabf52ec633239
KaMZeSs <vladd267@gmail.com>
2022-06-30 16:03:18 +0300

    wip

--- Reader/Books/FB2/Chapter.cs
+++ Reader/Books/FB2/Chapter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Reader.Books.FB2
 {
@@ -13,11 +14,43 @@ namespace Reader.Books.FB2
         {
             get { return hasChildren; }
         }
-
+        /*
+        Все говно
+        Лучше хранить все в одном массиве
+        Ладно название
+        Но весь текст, дочерние главы (так как название), изображения в одном массиве
+        Для этого нужно сделать интерфейс
+        public interface Obj {
+            что-то (можно забить и сделать ничего, а можно текст (название картинки, главы, текст абзаца))
+            можно еще сделать перечисление (запоминать тип для удобного сохранения)
+        }
+        а потом наследовать его для всех элементов
+        List<Obj> a = new()
+        a.Add(new Chapter);
+        a.Add(new Illustration);
+        и тд
+        */
         private Chapter[] children;
         public Chapter[] Children
         {
             get { return children; }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public Chapter()
+        {
+            hasChildren = false;
+            children = new Chapter[0];
+        }
+
+        public Chapter(XmlNode node)
+        {
+
         }
     }
 }
--- Reader/obj/Debug/net6.0-windows/App.g.i.cs
+++ Reader/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Reader {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Reader {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             Reader.App app = new Reader.App();
             app.InitializeComponent();
